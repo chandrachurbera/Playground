73122a356cff92a2289c83df0a3336d149ce2917
chandrachurbera <60910876+chandrachurbera@users.noreply.github.com>
2020-10-15 00:50:34 +0530

    Test code pushed to repository.

---
+++ My First C++ Program/Main.java
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+   printf("My First C++ Program");
+ //  printf("Hello, World!");
+   return 0;
+}
---
+++ Print from n to 0 [C++ Programming]/Main.java
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin>>n;
+    
+    unsigned int i = n+1;
+   
+    while(i > 0)
+    {
+      i--;
+    	cout<<i<<" ";
+      	
+    }
+   return 0;  
+}
